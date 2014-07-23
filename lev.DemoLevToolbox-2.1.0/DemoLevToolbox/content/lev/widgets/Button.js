97196764dde8271989e0c63beb5572b6dd0fcb4b
lzibarev <lzibarev@yandex.ru>
2014-07-23 15:36:19 +0400

    copy simple project

---
+++ lev.DemoLevToolbox-2.1.0/DemoLevToolbox/content/lev/widgets/Button.js
@@ -0,0 +1,136 @@
+// Button.js
+
+/**
+ * @class lev.widgets.Button
+ * @extends Ext.button.Button
+ * 
+ * @xtype lev_button_url
+ */
+Ext.define("lev.widgets.Button", {
+    extend: "Ext.button.Button",
+    alias: "widget.lev_button",
+    
+    config: {
+    	debug: false,
+    },
+
+    constructor: function(config) {
+        lev.widgets.Button.superclass.constructor.apply(this, [config]);
+    },
+
+	handler: function() {
+		this.log("clicked", arguments);
+		
+		if (this.runningInDesigner() == true) // don't perform button action if we're inside Designer
+			return;
+		
+		switch (this.externalURL.toLowerCase()) {
+		case 'back':
+			this.goBack();
+			break;
+			
+		case 'forward':
+			this.goForward();
+			break;
+			
+		case 'close':
+			this.goClose();
+			break;
+			
+		case 'reload':
+			this.goReload();
+			break;
+			
+		default:
+			this.openURL();
+		}
+		
+	},
+	
+	goBack: function() {
+		var iframe = this.iframe;
+		
+		if (iframe == null || iframe == undefined || iframe == "")
+			history.back();
+		else
+			window.frames[iframe].history.back();
+	},
+	
+	goForward: function() {
+		var iframe = this.iframe;
+		
+		if (iframe == null || iframe == undefined || iframe == "")
+			history.forward();
+		else
+			window.frames[iframe].history.forward();
+	},
+	
+	goClose: function() {
+		self.open('', '_self', '');
+		self.close();
+	},
+	
+	goReload: function() {
+		var iframe = this.iframe;
+		
+		if (iframe == null || iframe == undefined || iframe == "")
+			location.reload(true);
+		else
+			window.frames[iframe].location.reload(true);
+	},
+	
+	openURL: function() {
+		
+		var url = this.externalURL;
+		var iframe = this.iframe;
+		
+		if (iframe == null || iframe == undefined || iframe == "") {
+			
+			if (this.behavior == false)
+				location.href = url;
+			else
+				window.open(this.externalURL);
+			
+		} else {
+			
+			window.frames[iframe].location.href = url;
+			
+		}
+
+	},
+	
+	getValue: function() {
+		this.log("getValue");
+		return this.externalURL;
+	},
+	
+	setValue: function(url) {
+		this.log("setValue", arguments);
+		this.externalURL = url;
+		
+	},
+	
+    /** (custom)
+     * log messages to JavaScript console if debug==true
+     */
+    log: function() {
+    	if (this.getDebug() == true) {
+    		var id;
+    		
+    		if (this.id)
+    			id = this.id;
+    		else
+    			id = this.alias[0];
+    		
+    		console.log(id + ':', arguments);
+    	}
+    },
+    
+    runningInDesigner: function() {
+    	if (location.href.match(/http:\/\/127\.0\.0\.1:\d+\/uicomp/) == null)
+    		return false;
+    	else
+    		return true;
+
+    }
+});
---
+++ lev.DemoLevToolbox-2.1.0/DemoLevToolbox/content/lev/widgets/designer/Button.js
@@ -0,0 +1,47 @@
+/**
+ * @class lev.widgets.designer.Button
+ * @extends xcp.designer.Component
+ * Design time base component, wrapper runtime component.
+ * @constructor Creates a new design-time Button
+ * @param {cmp} runtime component.
+ */
+Ext.define('lev.widgets.designer.Button', {
+	extend: 'xcp.designer.Component',
+	constructor: function(cmp){
+		lev.widgets.designer.Button.superclass.constructor.call(this, cmp);
+
+		this.propertyConfig = {
+				"tabs": [{
+						"name": "general",
+						"sections": [{
+								"name" : "basic",
+								"properties": [{"name": "text"}, {"name": "iframe"}, {"name": "behavior"}, {"name": "debug"}]
+							},
+							{
+								"name" : "url",
+								"label": "URL",
+								"properties": [{"name": "externalURL"}]
+							}]
+					},
+                    {
+                        "name": xcp.Strings.widget.button.designer.Button.behaviorTabLabel,
+                        "sections":[{
+                            "name": "behavior",
+                            "label": xcp.Strings.widget.button.designer.Button.behaviorSectionLabel,
+                            "properties": [
+                                {"name": "hidden", "editor": "com.emc.xcp.uitemplate.ui.property.section.special.expr.BooleanExpressionPropertyEditor"},
+                                {"name": "disabled", "editor": "com.emc.xcp.uitemplate.ui.property.section.special.expr.BooleanExpressionPropertyEditor"}
+                            ]
+                        }]
+                    }]
+		};
+	},
+	updateComponent: function(propertiesJSONObject) {
+		//javaLog("xcp.widget.designer.Plaintext", "updateComponent", "log");
+        if (propertiesJSONObject && propertiesJSONObject.name === 'text') {
+		    this.cmp.setText(propertiesJSONObject.value);
+        }
+	}
+});
+
+xcp.designer.ComponentMgr.registerType("lev_button",lev.widgets.designer.Button);
