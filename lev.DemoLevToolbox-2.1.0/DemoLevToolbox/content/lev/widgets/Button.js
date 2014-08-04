0f99e41f127cd9e1ee2af81f0e4a0a9c34f83daf
lzibarev <lzibarev@yandex.ru>
2014-08-04 04:10:02 +0400

    v2.4

--- lev.DemoLevToolbox-2.1.0/DemoLevToolbox/content/lev/widgets/Button.js
+++ lev.DemoLevToolbox-2.1.0/DemoLevToolbox/content/lev/widgets/Button.js
@@ -1,14 +1,36 @@
+/*
+		    inputDataColumns: 'ДИТ,ДТЭ,ТехБлок,ЭкспФилиал',
+	    	inputDataStore: "ПЗ-Пояснительная записка,true,true,false,true;2-ПЗ-Пояснительная записка,false,true,true,true;3-ПЗ-Пояснительная записка,true,false,true,false",
+
+ */
 Ext.define("lev.widgets.Button", {
 	extend : "Ext.grid.Panel",
 	alias: "widget.lev_button",
 	requires : [ 'Ext.selection.CellModel', 'Ext.grid.*', 'Ext.data.*',
 			'Ext.util.*', 'Ext.form.*' ],
 		    xtype: 'cell-editing',
-		    frame: true,
 		    inputDataColumns: 'ДИТ,ДТЭ,ТехБлок,ЭкспФилиал',
 	    	inputDataStore: "ПЗ-Пояснительная записка,true,true,false,true;2-ПЗ-Пояснительная записка,false,true,true,true;3-ПЗ-Пояснительная записка,true,false,true,false",
+		    frame: true,
+		    debug: true,
 		    initComponent: function(){
 		    	this.height = 90;
+
+				this.setInputDataColumns(this.inputDataColumns);
+
+			    this.store = Ext.create('Ext.data.Store', {
+					fields : this.modelFields,
+					data : []
+				});
+
+				this.setInputDataStore(this.inputDataStore);
+
+				this.callParent(arguments);
+		    },
+		    
+		    setInputDataColumns: function(value){
+		    	this.log("setInputDataColumns "+value);
+			    this.inputDataColumns = value;
 		    	this.width = 350;
 			    this.columns = [ {
 					header : '№',
@@ -20,16 +42,13 @@ Ext.define("lev.widgets.Button", {
 					width : 300,
 					flex : 1
 				}];
-			    cNames = this.inputDataColumns.split(',');
 			    
 			    fields = ['number', 'code'];
-				modelFields = [
+				this.modelFields = [
 				        {name: 'number',  type: 'string'},
 				        {name: 'code', type: 'string'}
 				    ];
-				storeData = [];
-				
-				rows = this.inputDataStore.split(';');
+			    cNames = this.inputDataColumns.split(',');
 				for(index = 0; index < cNames.length; index++){
 			    	cName = cNames[index];
 			    	if (cName.length === 0){
@@ -42,33 +61,64 @@ Ext.define("lev.widgets.Button", {
 						dataIndex : cName});
 			    	this.width += 120;
 					fields.push(cName);
-					modelFields.push({name: cName, type: 'boolean'});
+					this.modelFields.push({name: cName, type: 'boolean'});
 			    }
+		    },
+		    
+		    updateInputDataColumns: function(value){
+			    this.log("updateInputDataColumns value="+value);
+		    	this.setInputDataColumns(value);
+		    	newStore = Ext.create('Ext.data.Store', {
+					fields : this.modelFields,
+					data : []
+				});
+		    	this.reconfigure(newStore, this.columns);
+		    },
+		    
+		    setInputDataStore: function(value){
+			    this.log("setInputDataStore value="+value);
+			    this.inputDataStore = value;
+				if (!this.inputDataStore){
+					return;
+				}	    
+			    this.store.removeAll();
+				rows = this.inputDataStore.split(';');
+				
 				for(j = 0; j < rows.length; j++){
 					row = rows[j];
 			    	if (row.length === 0){
 			    		continue;
 			    	}
 					rowFields = row.split(',');
-					console.log(rowFields);
-					storeRow = {'number': j+1, 'code': rowFields[0]}
-					storeData.push(storeRow);
+
+					storeRow = {'number': j+1}
+					
 					this.height+=25;
-					for(index = 0; index < cNames.length; index++){
-				    	cName = cNames[index];
-				    	storeRow[cName] = rowFields[1+index];
+					for(index = 1; index < this.modelFields.length; index++){
+				    	cName = this.modelFields[index].name;
+				    	storeRow[cName] = rowFields[index-1];
+				    	//console.log(cName+" "+(index-1)+" "+rowFields[index-1]);
 			    	}
-			    	console.log(storeRow);
+					this.store.add(storeRow);
 				}
-				    
-			    this.store = Ext.create('Ext.data.Store', {
-					fields : modelFields,
-					data : storeData
-				});
-				this.callParent(arguments);
+			    this.setHeight(this.height);
 		    },
+		    
+		    
+		    
 		    
 		    cellEditing: new Ext.grid.plugin.CellEditing({
 		            clicksToEdit: 1
-		        })
+		    	}), 
+		    
+		    log: function(message){
+		    	if (this.debug == true) {
+		    		var id;
+		    		if (this.id)
+		    			id = this.id;
+		    		else
+		    			id = this.alias[0];
+		    		console.log(id + ':', arguments);
+		    	}
+		    }
 });
